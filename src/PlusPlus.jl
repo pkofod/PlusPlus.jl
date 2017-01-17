9d5f338f3ea0ddf9c9532826577d50645d26f904
Simon <sdanisch@gmail.com>
2017-01-17 23:15:41 +0100

    add vcat

--- src/PlusPlus.jl
+++ src/PlusPlus.jl
@@ -9,4 +9,10 @@ Concatenates strings much better than *
 """
 ++(a, b) = a*b
 
+"""
+++(a, b...)
+Concatenates Vectors much better than *
+"""
+++(a::AbstractVector, b::AbstractVector...) = vcat(a, b...)
+
 end # module
